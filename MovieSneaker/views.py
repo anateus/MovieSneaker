e5936a652ec489724d078eeebea9b00c6a9d6ae6
Michael Katsevman <mikek@hlcam.com>
2012-09-20 18:51:35 -0700

    added redis

--- MovieSneaker/views.py
+++ MovieSneaker/views.py
@@ -1,11 +1,28 @@
-from flask import Flask, request, session, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, request, session, Response, render_template, json, jsonify
+from redis import Redis
+from redis import from_url as redis_from_url
 
-from . import app
+import os
+
+try:
+    from . import app
+except ValueError: # we're debugging
+    app = Flask(__name__)
+
+if os.path.exists('/home/dotcloud/environment.json'):
+    with open('/home/dotcloud/environment.json') as f:
+        env = json.load(f)
+else: # we're debugging
+    env = None
+
+if env:
+    redis = redis_from_url(env["DOTCLOUD_DATA_REDIS_URL"])
+else: # we're debugging
+    redis = Redis()
 
 @app.route('/')
 def index():
     return 'Good News Everyone!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
